bf0075aa80b4ffc705cf05622b3ac2daba7c8116
Stéphan Kochen <stephan@kochen.nl>
2016-06-26 11:28:01 +0200

    Tweak and comment build-config.lua.
    Only functional change here is the date format in the image config.
    Hopefully now Docker will understand it.

--- support/build-config.lua
+++ support/build-config.lua
@@ -1,12 +1,15 @@
--- Builds layer manifests and configs
+-- Builds layer manifests and configs.
 -- Usage: lua build-config.lua <repo> <architecture>
-
-local repo = arg[1]
-local arch = arg[2]
 
 local json = require('json')
 
-local creation_date = os.date("!%Y-%m-%dT%TZ")
+-- Parse arguments.
+if #arg ~= 2 then
+    os.exit(1)
+end
+local repo, arch = unpack(arg)
+
+-- Build the base config.
 local manifest = {
     {
         Config='config.json',
@@ -30,11 +33,16 @@ local config = {
     history={}
 }
 
+-- Format history `created` date.
+local creation_date = os.date("!%Y-%m-%dT%T.000000000Z")
+
+-- Iterate layers.
 local num = 0
 for line in io.open('_image/_meta.jsonl'):lines() do
     local layer = json.decode(line)
     num = num + 1
 
+    -- Add layer to config.
     manifest[1].RepoTags[1] = string.format('%s:%s-%s', repo, arch, layer.tag)
     manifest[1].Layers[num] = string.format('L%d/layer.tar', num)
     config.rootfs.diff_ids[num] = string.format('sha256:%s', layer.sha256)
@@ -43,10 +51,12 @@ for line in io.open('_image/_meta.jsonl'):lines() do
         created_by=string.format('/bin/sh -c #(nop) %s', layer.comment)
     }
 
+    -- Write layer manifest.
     local manifestf = io.open(string.format('_image/manifest-L%d.json', num), 'w')
     manifestf:write(json.encode(manifest))
     manifestf:close()
 
+    -- Write layer config.
     local configf = io.open(string.format('_image/config-L%d.json', num), 'w')
     configf:write(json.encode(config))
     configf:close()
